d698cffa41dfe69fd8647150e0a4d659d5a6ba5f
APlatko <104028000+APlatko@users.noreply.github.com>
2023-02-13 13:17:13 +0100

    add layouts

---
+++ nuxt/layouts/empty.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt/layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Page not found</h1>
+        <nuxt-link to="/">return to the home page</nuxt-link>
+    </div>
+</template>
+
+
+<script>
+export default {
+    layout: 'empty'
+}
+</script>
+
+<style scoped>
+
+</style>
